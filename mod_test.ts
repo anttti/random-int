a08827b1032fbcca95d53b9d66fce00e1f288c3d
Yogi <me@yogi.codes>
2020-06-09 10:52:16 +0530

    Update mod_test.ts

--- mod_test.ts
+++ mod_test.ts
@@ -1,6 +1,9 @@
 import { assert } from "https://deno.land/std/testing/asserts.ts";
 import { randomInt } from "./mod.ts";
 
+console.log(randomInt(10));
+console.log(randomInt(10, 1000));
+
 Deno.test("test randomInt function", async (): Promise<void> => {
   assert(Number(randomInt(10)));
   assert(Number(randomInt(10, 1000)));
